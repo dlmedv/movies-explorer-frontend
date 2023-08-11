6f779394b71904b2287171a06dca71aaefa5a381
dlmedv <114763924+dlmedv@users.noreply.github.com>
2023-08-11 11:56:48 +0300

    реализует функционал кнопки назад

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,13 +1,23 @@
 import './NotFound.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
 
 function NotFound() {
+
+    const navigate = useNavigate();
+  
+    const handleClick = (evt) => {
+      evt.preventDefault();
+      navigate(-1);
+    };
     return (
         <main>
             <section className='not-found'>
                 <h1 className='not-found__title'>404</h1>
                 <p className='not-found__text'>Страница не найдена</p>
-                <Link to='/' className='not-found__link'>Назад</Link>
+                <Link to='/' 
+                onClick={handleClick}
+                className='not-found__link'>Назад</Link>
             </section>
             </main>
     )
